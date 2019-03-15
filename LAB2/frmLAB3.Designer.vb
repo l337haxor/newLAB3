dc180ea1c72e09131c6a09a2859667e92cc0a140
l337haxor <47485167+l337haxor@users.noreply.github.com>
2019-03-15 16:39:40 -0400

    Initial commit

---
+++ LAB2/frmLAB3.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLAB3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ToolTipAverageShipped = New System.Windows.Forms.ToolTip(Me.components)
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.lblOutputEmployee3 = New System.Windows.Forms.Label()
+        Me.lblOutputEmployee2 = New System.Windows.Forms.Label()
+        Me.lblOutputEmployee1 = New System.Windows.Forms.Label()
+        Me.txtEmployee1 = New System.Windows.Forms.TextBox()
+        Me.txtEmployee2 = New System.Windows.Forms.TextBox()
+        Me.txtEmployee3 = New System.Windows.Forms.TextBox()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.lblEmployee1 = New System.Windows.Forms.Label()
+        Me.lblEmployee2 = New System.Windows.Forms.Label()
+        Me.lblEmployee3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(22, 343)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(94, 49)
+        Me.btnEnter.TabIndex = 19
+        Me.btnEnter.Text = "&Enter"
+        Me.ToolTipAverageShipped.SetToolTip(Me.btnEnter, "Click to calculate the semester average.")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(141, 343)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(94, 49)
+        Me.btnReset.TabIndex = 20
+        Me.btnReset.Text = "&Reset"
+        Me.ToolTipAverageShipped.SetToolTip(Me.btnReset, "Click to reset the form.")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(259, 343)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(94, 49)
+        Me.btnExit.TabIndex = 21
+        Me.btnExit.Text = "E&xit"
+        Me.ToolTipAverageShipped.SetToolTip(Me.btnExit, "Click to exit program.")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.ForeColor = System.Drawing.Color.Red
+        Me.lblOutput.Location = New System.Drawing.Point(22, 294)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(331, 23)
+        Me.lblOutput.TabIndex = 18
+        '
+        'lblOutputEmployee3
+        '
+        Me.lblOutputEmployee3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutputEmployee3.Location = New System.Drawing.Point(250, 246)
+        Me.lblOutputEmployee3.Name = "lblOutputEmployee3"
+        Me.lblOutputEmployee3.Size = New System.Drawing.Size(103, 30)
+        Me.lblOutputEmployee3.TabIndex = 27
+        Me.lblOutputEmployee3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOutputEmployee2
+        '
+        Me.lblOutputEmployee2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutputEmployee2.Location = New System.Drawing.Point(136, 246)
+        Me.lblOutputEmployee2.Name = "lblOutputEmployee2"
+        Me.lblOutputEmployee2.Size = New System.Drawing.Size(103, 30)
+        Me.lblOutputEmployee2.TabIndex = 26
+        Me.lblOutputEmployee2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblOutputEmployee1
+        '
+        Me.lblOutputEmployee1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutputEmployee1.Location = New System.Drawing.Point(22, 246)
+        Me.lblOutputEmployee1.Name = "lblOutputEmployee1"
+        Me.lblOutputEmployee1.Size = New System.Drawing.Size(103, 30)
+        Me.lblOutputEmployee1.TabIndex = 25
+        Me.lblOutputEmployee1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtEmployee1
+        '
+        Me.txtEmployee1.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.txtEmployee1.Location = New System.Drawing.Point(22, 117)
+        Me.txtEmployee1.Multiline = True
+        Me.txtEmployee1.Name = "txtEmployee1"
+        Me.txtEmployee1.ReadOnly = True
+        Me.txtEmployee1.Size = New System.Drawing.Size(103, 121)
+        Me.txtEmployee1.TabIndex = 28
+        '
+        'txtEmployee2
+        '
+        Me.txtEmployee2.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.txtEmployee2.Location = New System.Drawing.Point(136, 117)
+        Me.txtEmployee2.Multiline = True
+        Me.txtEmployee2.Name = "txtEmployee2"
+        Me.txtEmployee2.ReadOnly = True
+        Me.txtEmployee2.Size = New System.Drawing.Size(103, 121)
+        Me.txtEmployee2.TabIndex = 29
+        '
+        'txtEmployee3
+        '
+        Me.txtEmployee3.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.txtEmployee3.Location = New System.Drawing.Point(250, 117)
+        Me.txtEmployee3.Multiline = True
+        Me.txtEmployee3.Name = "txtEmployee3"
+        Me.txtEmployee3.ReadOnly = True
+        Me.txtEmployee3.Size = New System.Drawing.Size(103, 121)
+        Me.txtEmployee3.TabIndex = 30
+        '
+        'lblUnits
+        '
+        Me.lblUnits.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUnits.Location = New System.Drawing.Point(19, 63)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(62, 23)
+        Me.lblUnits.TabIndex = 31
+        Me.lblUnits.Text = "Units:"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(76, 67)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(49, 20)
+        Me.txtInput.TabIndex = 32
+        '
+        'lblDay
+        '
+        Me.lblDay.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDay.Location = New System.Drawing.Point(19, 24)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(106, 23)
+        Me.lblDay.TabIndex = 33
+        Me.lblDay.Text = "Day:"
+        '
+        'lblEmployee1
+        '
+        Me.lblEmployee1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmployee1.Location = New System.Drawing.Point(32, 94)
+        Me.lblEmployee1.Name = "lblEmployee1"
+        Me.lblEmployee1.Size = New System.Drawing.Size(115, 23)
+        Me.lblEmployee1.TabIndex = 34
+        Me.lblEmployee1.Text = "Employee &1"
+        '
+        'lblEmployee2
+        '
+        Me.lblEmployee2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmployee2.Location = New System.Drawing.Point(151, 94)
+        Me.lblEmployee2.Name = "lblEmployee2"
+        Me.lblEmployee2.Size = New System.Drawing.Size(115, 23)
+        Me.lblEmployee2.TabIndex = 35
+        Me.lblEmployee2.Text = "Employee &2"
+        '
+        'lblEmployee3
+        '
+        Me.lblEmployee3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmployee3.Location = New System.Drawing.Point(264, 94)
+        Me.lblEmployee3.Name = "lblEmployee3"
+        Me.lblEmployee3.Size = New System.Drawing.Size(115, 23)
+        Me.lblEmployee3.TabIndex = 36
+        Me.lblEmployee3.Text = "Employee &3"
+        '
+        'frmLAB3
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(374, 405)
+        Me.Controls.Add(Me.lblEmployee3)
+        Me.Controls.Add(Me.lblEmployee2)
+        Me.Controls.Add(Me.lblEmployee1)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.txtEmployee3)
+        Me.Controls.Add(Me.txtEmployee2)
+        Me.Controls.Add(Me.txtEmployee1)
+        Me.Controls.Add(Me.lblOutputEmployee3)
+        Me.Controls.Add(Me.lblOutputEmployee2)
+        Me.Controls.Add(Me.lblOutputEmployee1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lblOutput)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmLAB3"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average units shipped per employee"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolTipAverageShipped As ToolTip
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblOutputEmployee3 As Label
+    Friend WithEvents lblOutputEmployee2 As Label
+    Friend WithEvents lblOutputEmployee1 As Label
+    Friend WithEvents txtEmployee1 As TextBox
+    Friend WithEvents txtEmployee2 As TextBox
+    Friend WithEvents txtEmployee3 As TextBox
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblDay As Label
+    Friend WithEvents lblEmployee1 As Label
+    Friend WithEvents lblEmployee2 As Label
+    Friend WithEvents lblEmployee3 As Label
+End Class
---
+++ LAB2/frmLAB3.vb
@@ -0,0 +1,198 @@
+﻿Option Strict On
+
+Public Class frmLAB3
+
+    'Course: NETD-2202-01
+    'Assignment: Lab 3
+    'Name: Sterling Wenzelbach
+    'Date : Created: 2019-0-13; Modifed 2019-0-13
+    'Program description: The user will input 7 values in the input text box, one for each day of the week(7), for three employees. 
+    'Inputs are validated and must be a whole number, numeric and between 0 - 1000 (inclusive).
+    'Each valid input will be displayed vertically inside the corresponding employee output textbox.
+    'When 7 valid inputs are entered, the average will be displayed for that employee. This process is repeated for three employees.
+    'The overall average will then be diplayed in the output label, at this point the user can only reset the form or exit the program.
+
+    'DECLARATIONS
+    Public Const MINIMUM_VALUE As Integer = 0                               'stores the minimum value a user can input 
+    Public Const MAXIMUM_VALUE As Integer = 1000                            'stores the maximum value a user can input
+    Public Const MAX_DAYS As Integer = 7                                    'stores the maximum days/inputs that can be entered
+    Dim numberOfEmployees As Integer = 3
+    Dim arraySize As Integer = MAX_DAYS * numberOfEmployees
+    Dim arrayEmployees(numberOfEmployees - 1, MAX_DAYS - 1) As Integer      'create an array that holds 7 values
+    Dim dayNumber As Integer = 1                                            'create an array that holds 7 values
+    Dim arrayRowCounter As Integer = 0                                      'stores value for the array counter
+    Dim arrayColumnCounter As Integer = -1
+    Dim entry As String                                                     'stores input from txtInput
+    Dim total As Integer
+    Dim total1 As Integer                                                   'stores value for the total of the array values
+    Dim total2 As Integer                                                   'stores value for the total of the array values
+    Dim total3 As Integer                                                   'stores value for the total of the array values
+    Dim employee1Average As Double
+    Dim employee2Average As Double
+    Dim employee3Average As Double
+
+    Sub ResetForm()
+
+        'reset values        
+        ReDim arrayEmployees(numberOfEmployees - 1, MAX_DAYS - 1)
+        arrayRowCounter = 0                    'stores value for the array counter
+        arrayColumnCounter = -1
+        total = 0
+        total1 = 0
+        total2 = 0
+        total3 = 0
+        '============
+
+        'reset true/false 
+        txtInput.ReadOnly = False     'enable user from entering values into txtInput
+        btnEnter.Enabled = True    'enable the calculate button
+        txtInput.Enabled = True
+        txtInput.Select()
+        '================
+
+        'clear text/labels
+        txtEmployee1.Text = ""
+        txtEmployee2.Text = ""
+        txtEmployee3.Text = ""
+        lblOutput.Text = ""
+        txtInput.Text = ""
+        lblOutputEmployee1.Text = ""
+        lblOutputEmployee2.Text = ""
+        lblOutputEmployee3.Text = ""
+        '================
+    End Sub
+
+    Function ValidInput(ByVal input As String) As Boolean
+
+        Dim validOut As Integer
+        Dim validGrade As Boolean
+
+        If Integer.TryParse(input, validOut) Then
+            If validOut >= MINIMUM_VALUE And validOut <= MAXIMUM_VALUE Then
+                validGrade = True
+            Else
+                validGrade = False
+            End If
+        End If
+
+        Return validGrade
+
+    End Function
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        entry = txtInput.Text  'store the input into entry(string)  
+
+        If (ValidInput(entry)) Then                        'Call validation on entry and if true
+            If (dayNumber <= MAX_DAYS) Then                'If the day number is 7 or less
+                lblOutput.Text = ""                        'clear output label of any potential messages
+                arrayColumnCounter += 1                    'increase array column count by 1
+                dayNumber += 1                             'increase the day by 1
+
+                'check which employee is active to determine where to display the input text
+                'the row number determines this
+                If arrayRowCounter = 0 Then
+                    txtEmployee1.Text += entry & vbCrLf
+                ElseIf arrayRowCounter = 1 Then
+                    txtEmployee2.Text += entry & vbCrLf
+                Else
+                    txtEmployee3.Text += entry & vbCrLf
+                End If
+
+                'store entry value into the array [row,column]
+                arrayEmployees(arrayRowCounter, arrayColumnCounter) = CInt(entry)
+
+                lblDay.Text = "Day: " & dayNumber.ToString                          'change the day label
+                txtInput.Select()                                                   'set focus to the input text box
+
+                If dayNumber > MAX_DAYS Then                                        'if the day goes over 7
+                    dayNumber = 1                                                   'set day back to 1 for next employee
+                    lblDay.Text = "Day: " & dayNumber.ToString                      'change the day label
+
+                    'check which employee is active 
+                    'determine average for active employee
+                    If arrayRowCounter = 0 Then
+                        For Each value As Integer In arrayEmployees
+                            total1 += value
+                        Next
+                        'calculate the average; (total/7, in this case)
+                        employee1Average = total1 / MAX_DAYS
+                        'display the average, rounded to 2 decimals places
+                        lblOutputEmployee1.Text = "Average: " & Math.Round(employee1Average, 2)
+                        'change which employee label is bold
+                        lblEmployee1.Font = New Font(lblEmployee1.Font, FontStyle.Regular)
+                        lblEmployee2.Font = New Font(lblEmployee2.Font, FontStyle.Bold)
+
+                    ElseIf arrayRowCounter = 1 Then
+
+                        For Each value As Integer In arrayEmployees
+                            total2 += value
+                        Next
+                        'calculate the average; (this total minus the previous employee total) / 7
+                        employee2Average = (total2 - total1) / MAX_DAYS                     'calculate the average; (total/7, in this case)
+                        lblOutputEmployee2.Text = "Average: " & Math.Round(employee2Average, 2)
+                        lblEmployee2.Font = New Font(lblEmployee2.Font, FontStyle.Regular)
+                        lblEmployee3.Font = New Font(lblEmployee3.Font, FontStyle.Bold)
+
+                    Else
+
+                        For Each value As Integer In arrayEmployees
+                            total3 += value
+                        Next
+                        'calculate the average; (this total minus the previous employee total) / 7
+                        employee3Average = (total3 - total2) / MAX_DAYS                     'calculate the average; (total/7, in this case)
+                        lblOutputEmployee3.Text = "Average: " & Math.Round(employee3Average, 2)
+
+                        'Calculate total average of all employees
+                        Dim totalAverage As Double
+
+                        For Each value As Integer In arrayEmployees
+                            total += value
+                        Next
+                        'total of all employees values / size of the array (21)
+                        totalAverage = total / arraySize
+                        lblOutput.ForeColor = Color.Black
+                        lblOutput.Text = "The total average for all employees is: " & Math.Round(totalAverage, 2)
+
+                        'disable enter button and input textbox,set focus to reset button and clear value stored in entry
+                        btnEnter.Enabled = False
+                        txtInput.Enabled = False
+                        btnReset.Select()
+                        entry = ""
+
+                    End If
+                    'increase array row by 1(switch to next employee) and reset the column back to 0
+                    'ready for the next employees first value
+                    arrayRowCounter += 1
+                    arrayColumnCounter = -1
+
+                End If
+
+            End If
+        Else
+            'display error message if validation fails
+            txtInput.Select()
+            txtInput.SelectionStart = 0
+            txtInput.SelectionLength = Len(txtInput.Text)
+            lblOutput.ForeColor = Color.Red
+            lblOutput.Text = "ERROR: Input must be numeric and between " & MINIMUM_VALUE & "-" & MAXIMUM_VALUE & "."
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Call ResetForm() 'call the resetform subroutine (resets the program)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit() ' exit program
+    End Sub
+
+    Private Sub frmLAB3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'start the form off with the 'Employee 1' label as bold
+        lblEmployee1.Font = New Font(lblEmployee1.Font, FontStyle.Bold)
+        ''start the form off with the day label as "Day:1"
+        lblDay.Text = "Day: " & dayNumber.ToString
+
+    End Sub
+End Class
